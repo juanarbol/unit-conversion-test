e28b316437bfca56ffba15347938bdda1f07298c
Juan José Arboleda <soyjuanarbol@gmail.com>
2021-05-19 11:46:04 -0500

    First commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+type TempetureTypes = 'F' | 'C'
+
+export function temperuteConversion (temperature: number, desiredUnit: TempetureTypes): string {
+  let answer = 0;
+
+  switch (desiredUnit) {
+    case 'C':
+      answer = (temperature - 32) * (5/9)
+      break;
+    case 'F':
+      answer =  (9/5 * temperature) + 32
+      break;
+    default:
+      throw new Error('Provide a valid type, don\'t be like that with us!')
+  }
+
+  return `${answer}${desiredUnit}°`
+}
+
+export function doesNotConversion(): string {
+  return 'Noop'
+}
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+//import { temperuteConversion,  } from './index'
+import * as everything from './index'
+
+console.log(everything.temperuteConversion(23, 'F'))
+console.log(everything.doesNotConversion())
